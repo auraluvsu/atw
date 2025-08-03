5e25b68801959b28a8cfb1c0c94639a57d5df139
auraluvsu <ntreasure2015@gmail.com>
2025-08-03 11:45:06 +0100

    feat: connected status card and backend to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect} from 'react'
+import { WSMessage } from './types'
+import StatusCard from './components/StatusCard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [statusData, setStatusData] = useState<WSMessage | null>(null)
+    useEffect(() => {
+        const socket = new WebSocket(import.meta.env.VITE_WS_URL!);
+        socket.onopen = () => console.log('Connected to WebSocket server');
+        socket.onmessage = (event) => {
+            try {
+                const data = JSON.parse(event.data);
+                setStatusData(data);
+            } catch (err) {
+                console.error('Failed to parse WebSocket message:', err)
+            }
+        };
+        socket.onerror = (err) => console.error('WebSocket error:', err);
+        socket.onclose = () => console.log('WebSocket disconnected');
+        return () => socket.close();
+    }, []);
+    
+    return (
+        <div>
+            <h1>Real-Time Devops Dashboard</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            {!statusData ? (
+                <p>Loading Server data...</p>
+            ) : (
+                <div>
+                    {statusData.statuses.map((status) => (
+                        <StatusCard
+                        key={status.region}
+                        region={status.region}
+                        data={status.data}
+                        />
+                     ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App
